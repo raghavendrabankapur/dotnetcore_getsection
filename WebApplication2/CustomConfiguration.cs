fec00e4a5f5356c5c9d0edca116d7a3c7736ecd8
Raghavendra Bankapur <ba.raghu@gmail.com>
2018-07-03 22:17:33 +0530

    adding test project

---
+++ WebApplication2/CustomConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class CustomConfiguration
+    {
+        IConfiguration config = null;
+
+        public CustomConfiguration()
+        {
+            config = new ConfigurationBuilder()
+                .SetBasePath($"{Directory.GetCurrentDirectory()}")
+                .AddJsonFile($"data.json").Build();
+        }
+
+        public string Section(string key)
+        {
+            var data = config.GetSection(key);
+            return data.Value;
+        }
+    }
+}
